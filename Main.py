8bf9bf35d4348a86e471ac3b183eac2c01700c35
vlad <98947685+smilyanets@users.noreply.github.com>
2022-05-31 11:06:32 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,61 @@
+import arrLib as al
+
+# Виконано 22/33
+
+##########
+# ARRAYS #
+##########
+print("\n| main.py\n")
+def ARRAYS():
+
+    # Завдання 1 #
+    print(al.pairNumber("1 2 3 5 4 6 7"))
+
+    # Завдання 2 #
+    print(al.pairElements("1 2 3 5 4 6 7"))
+
+    # Завдання 3 #
+    print(al.plusElements("1 -2 -3 -5 4 6 7"))
+
+    # Завдання 4 #
+    print(al.moreThanPrevious("1 -2 -3 -5 4 6 7"))
+
+    # Завдання 5 #
+    print(al.someSings("1 -2 3 -3 1"))
+
+    # Завдання 6 #
+    print(al.moreThanTwoPrevious("1 -2 3 -3 1 0 -1"))
+    
+    # Завдання 7 #
+    print(al.flipArray("1 2 3 4 7 9"))
+
+    # Завдання 10 #
+    print(al.maxElement("1 -6 4 2"))
+
+    # Завдання 11 #
+    print(al.someElement("1 -6 1 1"))
+
+    # Завдання 12 #
+    print(al.lineArr(162))
+
+    # Завдання 15 #
+    print(al.balls("5 1 3 3 3 3 1 1 1 2"))
+
+    # Завдання 16 #
+    print(al.digionalArr(6))
+
+    # Завдання 17 #
+    print(al.symmetricalArray())
+
+    # Завдання 19 #
+    print(al.IIcompetition())
+
+    # Завдання 20 #
+    print(al.IIIcompetition())
+
+    # Завдання 21 #
+    print(al.IVcompetition())
+
+    # Завдання 22 #
+    print(al.Vcompetition())
+ARRAYS()
